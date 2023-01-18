0642f345b7365c57b53687a49e6ecb5bec372cc6
RubenCarton623 <rubenvera_77@hotmail.com>
2023-01-18 09:40:57 -0500

    Primer commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'pokemon.pokeapi.com',
+  appName: 'Pokemon App',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
